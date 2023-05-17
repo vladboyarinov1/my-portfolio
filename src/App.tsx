4327fa5df0e44a63c36880dc24e551bebed003e6
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-05-17 16:37:30 +0300

    refactor: add ready component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Header} from './components/Header/Header';
+import {Main} from './components/Main/Main';
+import {Skills} from './components/Skills/Skills';
+
 
 function App() {
-  return (
-    <div></div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Main/>
+            <Skills/>
+        </div>
+    );
 }
 
 export default App;
