e1840dffba269a0bfd4e91a231f1c5af08d75185
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-05-17 21:31:07 +0300

    refactor: add change

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Header} from './components/Header/Header';
 import {Main} from './components/Main/Main';
 import {Skills} from './components/Skills/Skills';
 import {Projects} from './components/Projects/Projects';
+import {Contacts} from './components/Contacts/Contacts';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Main/>
             <Skills/>
             <Projects/>
+            <Contacts/>
         </div>
     );
 }
