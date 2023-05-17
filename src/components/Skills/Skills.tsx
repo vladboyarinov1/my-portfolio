362128028018bb96abd5abbff9f3e6d10448c32f
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-05-17 20:22:29 +0300

    refactor: add changes

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import s from './Skills.module.css'
-import styleContsiner from '../../common/styles/Container.module.css'
+import styleContainer from '../../common/styles/Container.module.css'
 import {SkillItem} from './SkillItem/SkillItem';
 
 
 export const Skills = () => {
     return (
         <div className={s.skillsBlock}>
-            <div className={`${styleContsiner.container} ${s.skillsContainer}`}>
+            <div className={`${styleContainer.container} ${s.skillsContainer}`}>
                 <h2 className={s.title}>Skills</h2>
                 <div className={s.skills}>
                     <SkillItem/>
