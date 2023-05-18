55365e3521418db72f0ef6e8c8eb8f819fd93a65
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-05-18 14:43:38 +0300

    feat: add Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styleContainer from '../../common/styles/Container.module.css'
+import s from './Footer.module.css'
+import inst from '../../icons/instagram.svg'
+import facebook from '../../icons/facebook.svg'
+import telegram from '../../icons/telegram.svg'
+import linkedIn from '../../icons/linkedIn.svg'
+
+
+export const Footer = () => {
+    return (
+        <footer className={s.footerBlock}>
+            <div className={`${styleContainer.container} ${s.footerContainer}`}>
+                <p>Vladislav Boiarinov</p>
+                <div className={s.socialIcons}>
+                    <a href="https://www.instagram.com/your_profile" target="_blank">
+                        <img src={inst} alt="Instagram"/>
+                    </a>
+                    <a href="https://www.twitter.com/your_profile" target="_blank">
+                        <img src={telegram} alt="Twitter"/>
+                    </a>
+                    <a href="https://www.linkedin.com/your_profile" target="_blank">
+                        <img src={linkedIn} alt="LinkedIn"/>
+                    </a>
+                    <a href="https://www.facebook.com/your_profile" target="_blank">
+                        <img src={facebook} alt="Facebook"/>
+                    </a>
+                </div>
+                <p className={s.copyright}>© 2023 Все права защищены</p>
+            </div>
+        </footer>
+    );
+};
+
+
