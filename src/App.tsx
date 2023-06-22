3004f7fafb09dd3e3c5b1508993b5ab8fbbde9f7
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-06-22 21:38:50 +0300

    refactor: add changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Header} from './components/Header/Header';
-import {About} from './components/Main/About';
+import {About} from './components/About/About';
 import {Skills} from './components/Skills/Skills';
 import {Projects} from './components/Projects/Projects';
 import {Contacts} from './components/Contacts/Contacts';
