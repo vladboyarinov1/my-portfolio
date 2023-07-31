180267dae560adab2e8f0da740f481d13a458981
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-07-31 13:13:56 +0300

    refactor: add changes

--- src/components/About/Experience/Experience.tsx
+++ src/components/About/Experience/Experience.tsx
@@ -1,9 +1,13 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 
 export const Experience: FC<any> = () => {
-  return (
-    <div>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto ipsa, saepe. Cumque eveniet officia sint. Aliquam aliquid atque facere repellat ut vel vitae voluptatibus voluptatum? Accusantium aliquam, architecto asperiores blanditiis debitis eum excepturi fugit hic impedit ipsum labore magnam modi similique totam veritatis? Dolorem doloremque eligendi est expedita perspiciatis provident recusandae sunt tempora! Aspernatur blanditiis consequatur, ea libero omnis possimus praesentium ullam veniam voluptates. Quis.
-    </div>
-  );
+    return (
+        <div style={{color: '#939393', lineHeight: '1.6'}}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto ipsa, saepe. Cumque eveniet officia
+            sint. Aliquam aliquid atque facere repellat ut vel vitae voluptatibus voluptatum? Accusantium aliquam,
+            architecto asperiores blanditiis debitis eum excepturi fugit hic impedit ipsum labore magnam modi similique
+            totam veritatis? Dolorem doloremque eligendi est expedita perspiciatis provident recusandae sunt tempora!
+            Aspernatur blanditiis consequatur, ea libero omnis possimus praesentium ullam veniam voluptates. Quis.
+        </div>
+    );
 };
