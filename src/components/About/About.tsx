1a1d1ffb13a7ba27112973d6f3bd21203fac1c4b
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-08-06 18:56:04 +0300

    refactor: add changes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -35,50 +35,61 @@ export const About: FC<PropsType> = ({isDark}) => {
                             <img src={photo} alt=""/>
                         </div>
                     </Fade>
-                    <Fade right><div className={s.bioBlock}>
-                        <div className={s.textBlock}>
-                            <h2>My Bio</h2>
-                            <p>Hello and welcome to my portfolio website! My name is Vlad and I am a student at the
-                                Belarusian National Technical University, where I am studying "Software for Information
-                                Technologies". I am also taking courses at IT-INCUBATOR to deepen my knowledge in
-                                software development.</p>
-                            <p className={s.bioText}>My main area of focus in development is Frontend. I am confident in
-                                my knowledge of the modern technology stack, and I am ready to apply it to solve even
-                                the most complex tasks. My modern technology stack includes HTML, CSS, JavaScript,
-                                TypeScript, React, Redux-Saga, Formik, React-Query, Apollo GraphQL, Material UI, and
-                                Storybook. I also have experience working with React Native and REST API.
-                            </p>
-                            <p>Furthermore, I am a highly skilled athlete and have the title of Master of Sports of
-                                Belarus in Cross-Country Skiing. My sports career required the highest level of
-                                dedication. These valuable skills that I developed in sports also help
-                                me in working on software projects and achieving success in my professional
-                                endeavors.</p>
-                        </div>
-                        <div className={s.menu}>
-                            <NavWrapper isDark={isDark}><NavLink to="/skills"><p>Skills</p>
-                            </NavLink></NavWrapper>
-                            <NavWrapper isDark={isDark}><NavLink to="/experience"><p>Experience</p>
-                            </NavLink></NavWrapper>
-                            <NavWrapper isDark={isDark}><NavLink to="/education"><p>Education</p>
-                            </NavLink></NavWrapper>
-                        </div>
-                        <Routes>
-                            <Route path="/skills"
-                                   element={<Skills/>}/>
-                            <Route path="/experience"
-                                   element={<Experience/>}/>
-                            <Route path="/education"
-                                   element={<Education/>}/>
-                        </Routes>
-                        <div className={s.buttonsBlock}>
-                            <Link spy={true} smooth={true} offset={-70} duration={500} to="contacts"
-                                  className={s.hire}>HIRE
-                                ME</Link>
-                            <button className={s.download} onClick={downloadCV}>DOWNLOAD
-                                CV
-                            </button>
-                        </div>
-                    </div></Fade>
+                    <div className={s.bioBlock}>
+                        <Fade right>
+                            <div className={s.textBlock}>
+                                <h2>My Bio</h2>
+                                <p>Hello and welcome to my portfolio website! My name is Vlad and I am a student at the
+                                    Belarusian National Technical University, where I am studying "Software for
+                                    Information
+                                    Technologies". I am also taking courses at IT-INCUBATOR to deepen my knowledge in
+                                    software development.</p>
+                                <p className={s.bioText}>My main area of focus in development is Frontend. I am
+                                    confident in
+                                    my knowledge of the modern technology stack, and I am ready to apply it to solve
+                                    even
+                                    the most complex tasks. My modern technology stack includes HTML, CSS, JavaScript,
+                                    TypeScript, React, Redux-Saga, Formik, React-Query, Apollo GraphQL, Material UI, and
+                                    Storybook. I also have experience working with React Native and REST API.
+                                </p>
+                                <p>Furthermore, I am a highly skilled athlete and have the title of Master of Sports of
+                                    Belarus in Cross-Country Skiing. My sports career required the highest level of
+                                    dedication. These valuable skills that I developed in sports also help
+                                    me in working on software projects and achieving success in my professional
+                                    endeavors.</p>
+                            </div>
+                        </Fade>
+
+                        <Fade right>
+                            <div className={s.menu}>
+                                <NavWrapper isDark={isDark}><NavLink to="/skills"><p>Skills</p>
+                                </NavLink></NavWrapper>
+                                <NavWrapper isDark={isDark}><NavLink to="/experience"><p>Experience</p>
+                                </NavLink></NavWrapper>
+                                <NavWrapper isDark={isDark}><NavLink to="/education"><p>Education</p>
+                                </NavLink></NavWrapper>
+                            </div>
+
+                            <Routes>
+                                <Route path="/skills"
+                                       element={<Skills/>}/>
+                                <Route path="/experience"
+                                       element={<Experience/>}/>
+                                <Route path="/education"
+                                       element={<Education/>}/>
+                            </Routes></Fade>
+                        <Fade right>
+                            <div className={s.buttonsBlock}>
+                                <Link spy={true} smooth={true} offset={-70} duration={500} to="contacts"
+                                      className={s.hire}>HIRE
+                                    ME</Link>
+                                <button className={s.download} onClick={downloadCV}>DOWNLOAD
+                                    CV
+                                </button>
+                            </div>
+                        </Fade>
+                    </div>
+
                 </div>
             </main>
         </Element>
